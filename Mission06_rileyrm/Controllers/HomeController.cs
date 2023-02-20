0299f9da477ed09073ae00d9f38c66f41a5a3256
Riley Marshall <rileyrm@byu.edu>
2023-02-19 21:28:23 -0700

    Finished the update and delete features

--- Mission06_rileyrm/Controllers/HomeController.cs
+++ Mission06_rileyrm/Controllers/HomeController.cs
@@ -48,7 +48,8 @@ namespace Mission06_rileyrm.Controllers
             }
             else
             {
-                return View();
+                ViewBag.Categories = mrContext.categories.ToList();
+                return View(ar);
             }
         }
 
@@ -60,6 +61,44 @@ namespace Mission06_rileyrm.Controllers
                 .OrderBy(x => x.Title)
                 .ToList();
             return View(applications);
+        }
+        
+        [HttpGet]
+        public IActionResult Edit(int movieforumid)
+        {
+            ViewBag.Categories = mrContext.categories.ToList();
+
+            var application = mrContext.responses.Single(x => x.MovieForumID == movieforumid);
+
+            return View("MovieForum", application);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MovieForumResponse blah)
+        {
+            mrContext.Update(blah);
+            mrContext.SaveChanges();
+
+            return RedirectToAction("ViewMovies");
+
+            
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int movieforumid)
+        {
+            var application = mrContext.responses.Single(x => x.MovieForumID == movieforumid);
+            return View(application);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(MovieForumResponse blah)
+        {
+            mrContext.responses.Remove(blah);
+            mrContext.SaveChanges();
+
+
+            return RedirectToAction("ViewMovies");
         }
 
         
